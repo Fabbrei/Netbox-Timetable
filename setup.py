620a20ace2faaf3871c855facf5a56a783be85b7
fabbrei <fabrizio.dicesare@studio.unibo.it>
2023-09-01 12:21:49 +0200

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='netbox-timetable',
+    version='0.1',
+    description='Netbox Timetable management',
+    install_requires=[],
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False,
+)
