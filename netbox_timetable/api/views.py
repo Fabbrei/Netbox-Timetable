d53c88b291e3f882194a0080db1078bd8d53e532
fabbrei <fabrizio.dicesare@studio.unibo.it>
2023-09-01 12:29:39 +0200

    fixed Typo

--- netbox_timetable/api/views.py
+++ netbox_timetable/api/views.py
@@ -2,7 +2,7 @@ from django.db.models import Count
 from netbox.api.viewsets import NetBoxModelViewSet
 
 from .. import models
-from .serializers import CertificateSerializer, PowerSerializer
+from .serializers import CertificateSerializer
 
 
 class CertificateViewSet(NetBoxModelViewSet):
